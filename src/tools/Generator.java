92501b4b76d686fcf33dee25bee5c29e18feba21
Tommy Anderson <tommy2030@gmail.com>
2013-01-11 10:02:44 -0500

    added probability tools

---
+++ src/tools/Generator.java
@@ -0,0 +1,294 @@
+package tools;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Generator {
+	
+	static int[] ints = {1, 2, 3, 4, 5, 6, 7, 8,  9, 10, 11, 12, 13,
+						14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+						27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+						40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52};
+	
+	static TwoPlusTwo rankEngine; // helper class for determining hand rank/strength
+
+	public static void main(String[] args) throws IOException {
+		rankEngine = new TwoPlusTwo(); // load lookup table for hand ranks
+		
+		// example
+		System.out.println(getPreFlopOdds(stringToInt("Ks"), stringToInt("Kh"), stringToInt("Ad"))); // chances of winning preflop with three hole cards
+	}
+	
+	// helper function for generating card combinations -> removes cards from a list of possible cards
+	private static Integer[] getNewCards(int[] toRemove, int[] original) {
+		ArrayList<Integer> newCards = new ArrayList<Integer>();
+		for (int x = 0; x < original.length; x++) {
+			boolean add = true;
+			for (int y = 0; y < toRemove.length; y++) {
+				if (original[x] == toRemove[y]) {
+					add = false;
+					break;
+				}
+			}
+			if (add) {
+				newCards.add(original[x]);
+			}
+		}
+		return (Integer[]) newCards.toArray(new Integer[newCards.size()]);
+	}
+	
+	// helper function for converting card name to integer for the rankEngine (ie: As -> 52 or Ac -> 49)
+	public static int stringToInt(String card) {
+		String lower = card.toLowerCase();
+		String type = lower.substring(0, 1);
+		String suit = lower.substring(1);
+		
+		int base = 0;
+		if (type.equals("2")) {
+			base = 1;
+		} else if (type.equals("3")) {
+			base = 5;
+		} else if (type.equals("4")) {
+			base = 9;
+		} else if (type.equals("5")) {
+			base = 13;
+		} else if (type.equals("6")) {
+			base = 17;
+		} else if (type.equals("7")) {
+			base = 21;
+		} else if (type.equals("8")) {
+			base = 25;
+		} else if (type.equals("9")) {
+			base = 29;
+		} else if (type.equals("t")) {
+			base = 33;
+		} else if (type.equals("j")) {
+			base = 37;
+		} else if (type.equals("q")) {
+			base = 41;
+		} else if (type.equals("k")) {
+			base = 45;
+		} else if (type.equals("a")) {
+			base = 49;
+		}
+		
+		if (suit.equals("d")) {
+			base += 1;
+		} else if (suit.equals("h")) {
+			base += 2;
+		} else if (suit.equals("s")) {
+			base += 3;
+		}
+		
+		return base;
+	}
+
+	// given 3 hole cards and 3 flop cards, returns a boolean[] that indicates which hole card(s)
+	// 		should be discarded to maximize your chance of winning; ties/multiple-trues are possible
+	public static boolean[] findDiscard(int holeCardA, int holeCardB, int holeCardC, int boardCardA, int boardCardB, int boardCardC) {
+		long ABRank = 0;
+		long BCRank = 0;
+		long ACRank = 0;
+		
+		int[] cardsToRemove = {holeCardA, holeCardB, holeCardC, boardCardA, boardCardB, boardCardC};
+		Integer[] newCards = getNewCards(cardsToRemove, ints);
+		
+		for (int x = 0; x < 46; x++) {
+			int boardCardD = newCards[x];
+			for (int y = x+1; y < 46; y++) {
+				int boardCardE = newCards[y];
+				
+				int[] ABHand = {holeCardA, holeCardB, boardCardA, boardCardB, boardCardC, boardCardD, boardCardE};
+				int[] BCHand = {holeCardB, holeCardC, boardCardA, boardCardB, boardCardC, boardCardD, boardCardE};
+				int[] ACHand = {holeCardA, holeCardC, boardCardA, boardCardB, boardCardC, boardCardD, boardCardE};
+				
+				ABRank += rankEngine.lookupHand7(ABHand, 0);
+				BCRank += rankEngine.lookupHand7(BCHand, 0);
+				ACRank += rankEngine.lookupHand7(ACHand, 0);
+			}
+		}
+		
+		if (ABRank < 0 || BCRank < 0 || ACRank < 0) {
+			System.out.println("!!!Negative Value - Overflow!!!"); //sanity check
+		}
+		
+		long maxValue = ABRank;
+		if (BCRank > maxValue) {
+			maxValue = BCRank;
+		}
+		if (ACRank > maxValue) {
+			maxValue = ACRank;
+		}
+		
+		boolean[] toReturn = {false, false, false};
+		if (maxValue == ABRank) {
+			toReturn[2] = true;
+		}
+		if (maxValue == BCRank) {
+			toReturn[0] = true;
+		}
+		if (maxValue == ACRank) {
+			toReturn[1] = true;
+		}
+		
+		return toReturn;
+	}
+	
+	//given 3 hole cards, returns the chances for each hole card that you will end up discarding that card,
+	//		for all combinations of the flop to come
+	public static double[] getDiscardOdds(int holeCardA, int holeCardB, int holeCardC) {
+		long aCount = 0;
+		long bCount = 0;
+		long cCount = 0;
+		
+		boolean ABtie = false;
+		boolean BCtie = false;
+		boolean ACtie = false;
+		String ABCtie = "a";
+		
+		int[] cardsToRemove = {holeCardA, holeCardB, holeCardC};
+		Integer[] newCards = getNewCards(cardsToRemove, ints);
+		
+		for (int a = 0; a < 49; a++) {
+			int boardCardA = newCards[a];
+			for (int b = a+1; b < 49; b++) {
+				int boardCardB = newCards[b];
+				for (int c = b+1; c < 49; c++) {
+					int boardCardC = newCards[c];
+					
+					boolean[] toDiscard = findDiscard(holeCardA, holeCardB, holeCardC, boardCardA, boardCardB, boardCardC);
+					
+					if (toDiscard[0] && !toDiscard[1] && !toDiscard[2]) { // just a
+						aCount++;
+						continue;
+					}
+					
+					if (!toDiscard[0] && toDiscard[1] && !toDiscard[2]) { // just b
+						bCount++;
+						continue;
+					}
+					
+					if (!toDiscard[0] && !toDiscard[1] && toDiscard[2]) { // just c
+						cCount++;
+						continue;
+					}
+					
+					if (toDiscard[0] && toDiscard[1] && !toDiscard[2]) { // ABtie
+						if (ABtie) {
+							aCount++;
+						} else {
+							bCount++;
+						}
+						ABtie = !ABtie;
+						continue;
+					}
+					
+					if (!toDiscard[0] && toDiscard[1] && toDiscard[2]) { // BCtie
+						if (BCtie) {
+							bCount++;
+						} else {
+							cCount++;
+						}
+						BCtie = !BCtie;
+						continue;
+					}
+					
+					if (toDiscard[0] && !toDiscard[1] && toDiscard[2]) { // ACtie
+						if (ACtie) {
+							aCount++;
+						} else {
+							cCount++;
+						}
+						ACtie = !ACtie;
+						continue;
+					}
+					
+					if (toDiscard[0] && toDiscard[1] && toDiscard[2]) { // ABCtie
+						if (ABCtie.equals("a")) {
+							aCount++;
+							ABCtie = "b";
+						} else if (ABCtie.equals("b")) {
+							bCount++;
+							ABCtie = "c";
+						} else if (ABCtie.equals("c")) {
+							cCount++;
+							ABCtie = "a";
+						}
+					}
+				}
+			}
+		}
+		
+		double[] toReturn = {aCount/18424.0, bCount/18424.0, cCount/18424.0};
+		
+		return toReturn;
+		
+	}
+	
+	// given two hole cards that you'd like to keep and given one card you'd like to discard, returns the
+	// 		the chances of winning preFlop with that pair of hole cards, for all possible board and enemy card 
+	//		combinations to come (minus combinations that include our discardCard)
+	public static double getPreFlopDiscardOdds(int holeCardA, int holeCardB, int discardCard) throws IOException {
+		long wins = 0;
+		long gameCount = 0;
+		
+		int[] cardsToRemove = {holeCardA, holeCardB, discardCard};
+		Integer[] newCards = getNewCards(cardsToRemove, ints);
+		int[] newCardsArray = new int[49];
+		for (int z = 0; z < 49; z++) {
+			newCardsArray[z] = newCards[z];
+		}
+		
+		for (int a = 0; a < 49; a++) {
+			int enemyCardA = newCards[a];
+			for (int b = a+1; b < 49; b++) {
+				int enemyCardB = newCards[b];
+				int[] newCardsToRemove = {enemyCardA, enemyCardB};
+				Integer[] finalNewCards = getNewCards(newCardsToRemove, newCardsArray);
+				
+				for (int c = 0; c < 47; c++) {
+					int boardCardA = finalNewCards[c];
+					for (int d = c+1; d < 47; d++) {
+						int boardCardB = finalNewCards[d];
+						for (int e = d+1; e < 47; e++) {
+							int boardCardC = finalNewCards[e];
+							for (int f = e+1; f < 47; f++) {
+								int boardCardD = finalNewCards[f];
+								for (int g = f+1; g < 47; g++) {
+									int boardCardE = finalNewCards[g];
+									
+									int[] myHand = {holeCardA, holeCardB, boardCardA, boardCardB, boardCardC, boardCardD, boardCardE};
+									int[] enemyHand = {enemyCardA, enemyCardB, boardCardA, boardCardB, boardCardC, boardCardD, boardCardE};
+									
+									int myRank = rankEngine.lookupHand7(myHand, 0);
+									int enemyRank = rankEngine.lookupHand7(enemyHand, 0);
+									
+									if (myRank >= enemyRank) {
+										wins++;
+									}
+									
+									gameCount++;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		return ((wins*1.0)/gameCount);
+	}
+	
+	// given three hole cards, returns the chances of winning preFlop
+	private static double getPreFlopOdds(int holeCardA, int holeCardB, int holeCardC) throws IOException {
+		double termAB = getPreFlopDiscardOdds(holeCardA, holeCardB, holeCardC);
+		double termBC = getPreFlopDiscardOdds(holeCardB, holeCardC, holeCardA);
+		double termAC = getPreFlopDiscardOdds(holeCardA, holeCardC, holeCardB);
+		
+		double[] odds = getDiscardOdds(holeCardA, holeCardB, holeCardC);
+		
+		return (termAB*odds[2] + termBC*odds[0] + termAC*odds[1]);
+	}
+	
+}
---
+++ src/tools/TwoPlusTwo.java
@@ -0,0 +1,106 @@
+package tools;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+ 
+/**
+ * For more information about this algorithm, see
+ * http://archives1.twoplustwo.com/showflat.php?Cat=0&Number=8513906&page=0&fpart=1&vc=1
+ * @author Chris Oei http://www.linkedin.com/in/christopheroei
+ */
+public class TwoPlusTwo {
+ 
+	
+    /**
+     * HandRanks.dat
+     * http://static.eluctari.com/download/game/poker/HandRanks.dat<br/>
+     * Size: 129951336<br/>
+     * CRC32: 7808da57<br/>
+     * MD5: 5de2fa6f53f4340d7d91ad605a6400fb<br/>
+     * SHA1: f8467e36f470c9beea98c47d661c9b2c4a13e577<br/>
+     * SHA256: ad00f3976ad278f2cfd8c47b008cf4dbdefac642d70755a9f20707f8bbeb3c7e<br/>
+     */
+    private static final String HAND_RANK_DATA_FILENAME = "HandRanks.dat";
+    private static final int HAND_RANK_SIZE = 32487834;
+    private static int HR[] = new int[HAND_RANK_SIZE];
+    public static String[] HAND_RANKS = {"BAD!!", "High Card", "Pair", "Two Pair", "Three of a Kind",
+    	"Straight", "Flush", "Full House", "Four of a Kind", "Straight Flush"};
+ 
+    /**
+     * Calculate the poker hand rank of seven cards.
+     * @param cards		an integer array
+     * @param offset	evaluate the next seven cards starting at this position
+     * @return			the hand rank
+     */
+    public static int lookupHand7(int[] cards, int offset) {
+        int pCards = offset;
+        int p = HR[53 + cards[pCards++]];
+        p = HR[p + cards[pCards++]];
+        p = HR[p + cards[pCards++]];
+        p = HR[p + cards[pCards++]];
+        p = HR[p + cards[pCards++]];
+        p = HR[p + cards[pCards++]];
+        return HR[p + cards[pCards]];
+    }
+ 
+    /**
+     * Calculate the poker hand rank of five cards.
+     * @param cards		an integer array
+     * @return			the hand rank
+     */
+    public static int lookupHand5(int[] cards) {
+        int pCards = 0;
+        int p = HR[53 + cards[pCards++]];
+        p = HR[p + cards[pCards++]];
+        p = HR[p + cards[pCards++]];
+        p = HR[p + cards[pCards++]];
+        p = HR[p + cards[pCards]];
+        p = HR[p];
+        return HR[p];
+    }
+    
+    /**
+     * Converts a little-endian byte array to a Java (big-endian) integer.
+     * We need this because the HandRanks.dat file was generated using
+     * a little-endian C program and we want to maintain compatibility.
+     * @param b
+     * @param offset
+     * @return
+     */
+    private static final int littleEndianByteArrayToInt(byte[] b, int offset) {
+        return (b[offset + 3] << 24) + ((b[offset + 2] & 0xFF) << 16)
+                + ((b[offset + 1] & 0xFF) << 8) + (b[offset] & 0xFF);
+    }
+ 
+    static {
+        int tableSize = HAND_RANK_SIZE * 4;
+        byte[] b = new byte[tableSize];
+        InputStream br = null;
+        try {
+            br = new BufferedInputStream(new FileInputStream(HAND_RANK_DATA_FILENAME));
+            int bytesRead = br.read(b, 0, tableSize);
+            if (bytesRead != tableSize) {
+            	System.out.println("TableError");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getStackTrace());
+        } catch (IOException e) {
+        	System.out.println(e.getStackTrace());
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+            	System.out.println(e.getStackTrace());
+            }
+        }
+        for (int i = 0; i < HAND_RANK_SIZE; i++) {
+            HR[i] = littleEndianByteArrayToInt(b, i * 4);
+        }
+        System.out.println("Rank lookup table initialized");
+
+    }
+    
+}
